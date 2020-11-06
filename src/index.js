9b74c32007ff244debcf0a12c06bd99e3421eb12
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-06 18:14:02 +0530

    initial code

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>This is React Page</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
