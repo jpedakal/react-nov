df90bf4bc29196dbbcb96deef10da1964bb43af5
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-16 10:22:04 +0530

    update

---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <hr />
+            <h1>This is React Page</h1>
+        </div>
+    )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Header from './Header'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './component/Home'
 
-const App = () => {
-    return (
-        <div>
-            <Header />
-            <hr />
-            <h1>This is React Page</h1>
-        </div>
-    )
-}
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
