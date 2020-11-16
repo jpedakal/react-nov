8ded11b027ac5ba7e4ce537566f7065ffd31f21e
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-16 10:37:24 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import JSON from '../db.json'
 
-const Home = () => {
-    return (
-        <div>
-            <Header />
-            <hr />
-            <h1>This is React Page</h1>
-        </div>
-    )
+class Home extends Component {
+    
+    constructor(){
+        super()
+
+        this.state={
+            news: JSON
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <hr />
+                <h1>This is React Page</h1>
+            </div>
+        )
+    }
 }
 
 export default Home;
