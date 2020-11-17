848a246a67d695d68ff6856781e5cf63c7b4f396
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-17 19:48:21 +0530

    update

---
+++ src/component/NewsDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDisplay = () => {
+    return (
+        <div>
+            <h2>React Application</h2>
+        </div>
+    )
+}
+
+export default NewsDisplay;
