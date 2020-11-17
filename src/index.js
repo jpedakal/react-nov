dd81aa96147855ca68fadb4be7f1dbb4ab4aa527
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-17 17:37:59 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './component/Home'
+import Home from './component/Home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
