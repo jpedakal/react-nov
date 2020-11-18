7504890a6a5d5cc1e69c293718d835fa36430dd0
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-11-18 20:08:14 +0530

    Update NewsDisplay.js

--- src/component/NewsDisplay.js
+++ src/component/NewsDisplay.js
@@ -2,14 +2,17 @@ import React from 'react';
 import LazyLoad from 'react-lazyload';
 
 const NewsDisplay = (props) => {
-    
+
     const renderList = props.datalist.map((data) => {
         return (
-            <div>
-                <h2>{data.title}</h2>
-                <p>{data.feed}</p>
-                <img src={data.imageUrl} alt="figure" />
-            </div>
+            <LazyLoad height={200} throttle={1000}>
+                <div>
+                    <h2>{data.title}</h2>
+                    <p>{data.feed}</p>
+                    <img src={data.imageUrl} alt="figure" />
+                </div>
+            </LazyLoad>
+
         )
     })
 
